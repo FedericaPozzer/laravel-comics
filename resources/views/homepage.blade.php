0f35152f029018e197d7d5c14a29a99de9ce48f1
Federica Pozzer <federica.pozzer@gmail.com>
2023-03-28 19:14:00 +0200

    database boh

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,49 +1,60 @@
 @extends("layouts.app")
 
 @section("main") 
-ciao sono il main della homepage
 
+<section class="my-3">
+    <div class="container">
+        <div class="row row-cols-6 text-light">
+            <div class="col">
+                <div>
+                    <img src="" alt="img">
+                </div>
+                <div>
+                    NOME COMIC
+                </div>
+            </div>
+        </div>
+    </div>
 
-
-
-
-
-
+    <div class="w-100 d-flex justify-content-center">
+        <button>LOAD MORE</button>
+    </div>
+</section>
 
 <section class="section-links text-light">
-        <ul class="container d-flex justify-content-between align-items-center">
+     <ul class="container d-flex justify-content-between align-items-center">
              {{-- <li v-for="link of links" v-bind:key="link.number">
                 <img :src="getImagePath(link.icon)" alt="icon">
                 <a href="#"> {{ link.name }} </a>
             </li> --}}
 
-            <li>
-                <img src="../assets/img/buy-comics-digital-comics.png" alt="icon">
-                <div>DIGITAL COMICS</div> 
-            </li>
+        <li>
+            <img src="../assets/img/buy-comics-digital-comics.png" alt="icon">
+            <div>DIGITAL COMICS</div> 
+        </li>
 
-            <li>
-                <img src="../assets/img/buy-comics-merchandise.png" alt="icon">
-                <div>DC MERCHANDISE</div>
-            </li>
+        <li>
+            <img src="../assets/img/buy-comics-merchandise.png" alt="icon">
+            <div>DC MERCHANDISE</div>
+        </li>
 
-            <li>
-                <img src="../assets/img/buy-comics-subscriptions.png" alt="icon">
-                <div>SUBSCRIPTION</div>
-            </li>
+        <li>
+            <img src="../assets/img/buy-comics-subscriptions.png" alt="icon">
+            <div>SUBSCRIPTION</div>
+        </li>
 
-            <li>
-                <img src="../assets/img/buy-comics-shop-locator.png" alt="icon">
-                <div>COMIC SHOP LOCATOR</div>
-            </li>
+        <li>
+            <img src="../assets/img/buy-comics-shop-locator.png" alt="icon">
+            <div>COMIC SHOP LOCATOR</div>
+        </li>
 
-            <li>
-                <img src="../assets/img/buy-dc-power-visa.svg" alt="icon" class="icon-smaller">
-                <div>DC POWER VISA</div>
-            </li> 
+        <li>
+            <img src="../assets/img/buy-dc-power-visa.svg" alt="icon" class="icon-smaller">
+            <div>DC POWER VISA</div>
+        </li> 
             
-        </ul>
-    </section>
+    </ul>
+</section>
 
 
 @endsection
